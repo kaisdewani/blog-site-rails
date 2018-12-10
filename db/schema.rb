b7ea74183a5ca244bcc0f4baacb592a907ae1a70
Kais Dewani <kaisdewani@yahoo.com>
2018-12-10 16:35:18 -0500

    add description, created at, and updated at columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_212657) do
+ActiveRecord::Schema.define(version: 2018_12_10_213141) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
